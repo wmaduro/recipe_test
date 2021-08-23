ce15f7c92a06c64c52c23d1580e07c6b10a79a4a
Welerson Maduro <wmaduro@gmail.com>
2021-08-23 14:10:03 +1200

    recipe detail page format  - preliminary

--- lib/context/recipe-detail/view/recipe_detail_page.dart
+++ lib/context/recipe-detail/view/recipe_detail_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 import 'package:flutter/material.dart';
 import 'package:recipe_test/context/recipe-detail/controller/recipe_detail_controller.dart';
+import 'package:recipe_test/context/recipe-list/models/ingredient.dart';
 import 'package:recipe_test/context/recipe-list/models/recipe_dto.dart';
 import 'package:recipe_test/core/utils/color_util.dart';
 import 'package:rx_notifier/rx_notifier.dart';
@@ -36,7 +37,57 @@ class RecipeDetailPageState extends State<RecipeDetailPage> {
 
   _body(BuildContext context) {
     return Container(child: RxBuilder(builder: (_) {
-      return Text('${recipeDetailController.rxRecipeDTO?.name}');
+      return Column(
+        children: [
+          _showHeader(
+              recipeDetailController.rxRecipeDTO?.imageUrl,
+              recipeDetailController.rxRecipeDTO?.name,
+              recipeDetailController.rxRecipeDTO?.getTotalTime()),
+          SizedBox(
+            height: 5,
+          ),
+          _showIngredients(recipeDetailController.rxRecipeDTO?.ingredients),
+          SizedBox(
+            height: 5,
+          ),
+          _showSteps(recipeDetailController.rxRecipeDTO?.steps),
+        ],
+      );
     }));
+  }
+
+  _showHeader(String? imageUrl, String? name, int? totalTime) {
+    return Column(children: [
+      Text(
+        name ?? '',
+        style: TextStyle(fontSize: 25),
+      ),
+      Text(
+        imageUrl ?? '',
+        style: TextStyle(fontSize: 25),
+      ),
+      Text(
+        '${totalTime ?? 0}',
+        style: TextStyle(fontSize: 25),
+      ),
+    ]);
+  }
+
+  _showIngredients(List<Ingredient>? ingredients) {
+    var widgetList = <Widget>[];
+    ingredients?.forEach((element) {
+      widgetList.add(Text('${element.quantity} ${element.name}'));
+    });
+
+    return Column(children: widgetList);
+  }
+
+  _showSteps(List? steps) {
+    var widgetList = <Widget>[];
+    steps?.forEach((element) {
+      widgetList.add(Text('${element}'));
+    });
+
+    return Column(children: widgetList);
   }
 }
